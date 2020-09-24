5163dc079e53771872c97586a7db185416cba305
John Wong <jwwwwong@gmail.com>
2020-09-24 18:34:27 +0800

    Fix createWIthdrawal example

--- examples/createWithdrawal.js
+++ examples/createWithdrawal.js
@@ -6,13 +6,13 @@ const BigNumber = require('bignumber.js')
 
 async function createWithdrawal() {
   const mnemonic = 'direct social glue family aspect vanish hollow grocery armed goat fortune chapter'
-  const accountWallet = await Wallet.connect(mnemonic)
+  const accountWallet = await Wallet.connect(mnemonic, 'DEVNET')
   const params = {
-    to_address: '6bae56C7C534c38E08564C4b482a04Ea53B7A29c',
-    denom: 'neo',
-    amount: new BigNumber('10').toFixed(18),
-    fee_amount: new BigNumber('1').toFixed(18),
-    fee_address: 'swth142ph88p9ju9wrmw65z6edq67f20p957m92ck9d',
+    to_address: '6ee8ff54dda23d4c9feeca474794bd077ddf96d1',
+    denom: 'swth2',
+    amount: new BigNumber('0.00000010').toFixed(18),
+    fee_amount: new BigNumber('0.00000001').toFixed(18),
+    fee_address: 'swth1prv0t8j8tqcdngdmjlt59pwy6dxxmtqgycy2h7',
   }
   const result = await api.createWithdrawal(accountWallet, params)
   console.log('result', result)
