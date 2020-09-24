da216ac39fe5ef1f962b36d0e64bc5faf3fda699
John Wong <jwwwwong@gmail.com>
2020-09-24 18:33:30 +0800

    Update createWithdrawal example

--- examples/createWithdrawal.js
+++ examples/createWithdrawal.js
@@ -8,11 +8,11 @@ async function createWithdrawal() {
   const mnemonic = 'direct social glue family aspect vanish hollow grocery armed goat fortune chapter'
   const accountWallet = await Wallet.connect(mnemonic)
   const params = {
-    ToAddress: '6bae56C7C534c38E08564C4b482a04Ea53B7A29c',
-    Denom: 'neo',
-    Amount: new BigNumber('10').toFixed(18),
-    FeeAmount: new BigNumber('1').toFixed(18),
-    FeeAddress: 'swth142ph88p9ju9wrmw65z6edq67f20p957m92ck9d',
+    to_address: '6bae56C7C534c38E08564C4b482a04Ea53B7A29c',
+    denom: 'neo',
+    amount: new BigNumber('10').toFixed(18),
+    fee_amount: new BigNumber('1').toFixed(18),
+    fee_address: 'swth142ph88p9ju9wrmw65z6edq67f20p957m92ck9d',
   }
   const result = await api.createWithdrawal(accountWallet, params)
   console.log('result', result)
