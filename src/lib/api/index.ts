70a13ed7789e14e32916b7544db61a4a9acc7d26
junwei <chanjunwei@gmail.com>
2020-09-25 19:29:22 +0800

    fix exports

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,16 +1,6 @@
 export { mintTokens, MintTokenRequest } from './mints'
-export {
-    createToken, createTokens, CreateTokenMsg,
-    mintTestnetTokens, MintTokenMsg, mintMultipleTestnetTokens,
-    sendTokens
-} from './tokens'
-export { createOracle, CreateOracleMsg, createVote, CreateVoteMsg } from './oracles'
 export { createWithdrawal, CreateWithdrawalMsg } from './withdrawal'
-export * from './staking'
 export { createSubAccount, activateSubAccount } from './subaccount'
-export { getPrices, getIndexPrice, getLastPrice } from './prices'
-export * from './amm'
-export { submitProposal, depositProposal, voteProposal, SubmitProposalMsg, DepositProposalMsg, VoteProposalMsg } from './gov'
 
 export {
     WsWrapper,
