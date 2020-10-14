dfc512dc8119e3e657577d61479ebd20ea99170a
Ivan Poon <ravenxce@gmail.com>
2020-10-14 16:13:42 +0800

    Update examples

--- examples/createDeposit.js
+++ examples/createDeposit.js
@@ -8,18 +8,18 @@ async function createDeposit() {
   const wallet = await WalletClient.connectMnemonic(mnemonics[0], process.env.NET) // this is the receiving addr
   const params = {
     name: 'Switcheo NEP-5',
-    symbol: 'SWTHNV2',
-    denom: 'swth-n-v2',
+    symbol: 'SWTHN',
+    denom: 'swth-n',
     decimals: '8',
     native_decimals: '8',
     blockchain: 'neo',
     chain_id: '4',
-    asset_id: 'bce14688890823e90ccd2119b23cd7a212aca08b',
+    asset_id: 'a0bbd75a5a6d4cbefe9499eba0c8e108f3d24705',
     is_collateral: false,
-    lock_proxy_hash: '092cec2fd8e88693dc068a30176d667711df39e3',
+    lock_proxy_hash: '206a5dfd13955c4d66a012d748bba4bbee79b7bb',
     delegated_supply: '0',
-    originator: 'swth1lp5tsyq623gxd0q496v5u8jrvpfgu2lcks6zun',
-    externalBalance: '7000000000' // 70.00000000
+    originator: 'swth1phsut994s4e9apdc25vdln02g9rz7exz2fr92n',
+    externalBalance: '8800000000' // 88.00000000
   }
   await wallet.sendNeoDeposit(params, process.env.PRIVATE_KEY).then(console.log) // this is the sending addr
 }
--- examples/createToken.js
+++ examples/createToken.js
@@ -1,5 +1,5 @@
 // const SDK = require('switcheo-chain-js-sdk') // use this instead if running this sdk as a library
-const SDK = require('../.')
+const SDK = require('../build/main')
 const { clients } = SDK
 const { WalletClient, RestClient } = clients
 const mnemonics = require('../mnemonics.json')
@@ -8,15 +8,15 @@ async function createToken() {
   const wallet = await WalletClient.connectMnemonic(mnemonics[0], process.env.NET)
   const params = {
     name: 'Switcheo NEP-5',
-    symbol: 'SWTHNV2',
-    denom: 'swth-n-v2',
+    symbol: 'SWTHN',
+    denom: 'swth-n',
     decimals: '8',
     native_decimals: '8',
     blockchain: 'neo',
     chain_id: '4',
-    asset_id: 'bce14688890823e90ccd2119b23cd7a212aca08b',
+    asset_id: 'a0bbd75a5a6d4cbefe9499eba0c8e108f3d24705',
     is_collateral: false,
-    lock_proxy_hash: '092cec2fd8e88693dc068a30176d667711df39e3',
+    lock_proxy_hash: '206a5dfd13955c4d66a012d748bba4bbee79b7bb',
     delegated_supply: '0',
   }
   const rest = new RestClient({ network: process.env.NET, wallet })
