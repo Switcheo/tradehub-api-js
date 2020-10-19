2f7d89d73d04d3032ec5e89afa4d8c9f74a00b74
junwei <chanjunwei@gmail.com>
2020-10-19 17:28:14 +0800

    update amm examples

--- examples/linkPool.js
+++ examples/linkPool.js
@@ -8,7 +8,7 @@ function sleep(ms) {
 
 const network = 'LOCALHOST'
 
-async function createPool() {
+async function linkPool() {
   const wallet = await setupAccount(process.env.MNEMONICS)
   const minterClient = new RestClient({ wallet, network })
   const account = newAccount(network)
@@ -31,17 +31,16 @@ async function createPool() {
   const mintResult = await minterClient.mintMultipleTestnetTokens(tokenReq)
   console.log('mintResult', mintResult)
   await sleep(1000)
+
   const accountWallet = await WalletClient.connectMnemonic(account.mnemonic, network)
   const mainClient = new RestClient({ wallet: accountWallet, network})
-  console.log('creating pool')
+  console.log('linking pool')
   const params = {
-    token_a_denom: 'eth',
-    token_a_weight: '90',
-    token_b_denom: 'btc',
-    token_b_weight: '10',
+    pool_id: '1',
+    market: 'swth_eth',
 
   }
-  mainClient.createPool(params).then(console.log)
+  mainClient.unlinkPool(params).then(console.log)
 }
 
-createPool()
+linkPool()
