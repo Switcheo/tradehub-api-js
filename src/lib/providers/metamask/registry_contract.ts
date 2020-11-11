07c5d9726f4c36b4e27752c20eb759e8195f354f
Steven Khong <khongwh@gmail.com>
2020-11-11 20:05:31 +0800

    Add metamask login provider

---
+++ src/lib/providers/metamask/registry_contract.ts
@@ -0,0 +1,46 @@
+export default {
+  abi: [
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "map",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "ethEncKey",
+          "type": "string"
+        }
+      ],
+      "name": "Store",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+}
