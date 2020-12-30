e1bb7f94cc2db93c19852347aef952eabd053b18
PC <spclight@gmail.com>
2020-12-30 15:24:22 +0800

    Add create market proposal

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -424,7 +424,7 @@ export interface RemoveDebtMsg {
 export type ProposalValue = ProposalParameterChangeValue | ProposalTokenValue |
   ProposalMarketValue | ProposalSettlementPriceValue | ProposalOracleValue | ProposalOracleResultValue |
   ProposalLinkPoolValue | ProposalUnlinkPoolValue | ProposalCreateTokenValue | ProposalSetPoolRewardWeightsValue |
-  ProposalSetCommitmentCurveValue
+  ProposalSetCommitmentCurveValue | ProposalCreateMarketValue
 
 export interface ProposalParameterChangeValue {
   title: string,
@@ -578,6 +578,36 @@ export interface ProposalSetCommitmentCurveValue {
       max_reward_multiplier: number,
       originator: string,
     }
+}
+
+export interface ProposalCreateMarketValue {
+  title: string,
+  description: string,
+  market: {
+    name: string,
+    display_name: string,
+    market_type: 'spot' | 'futures',
+    description: string,
+    base: string,
+    quote: string,
+    lot_size: string,
+    tick_size: string,
+    min_quantity: string,
+    risk_step_size: string,
+    initial_margin_base: string,
+    initial_margin_step: string,
+    maintenance_margin_ratio: string,
+    max_liquidation_order_ticket: string,
+    max_liquidation_order_duration: string,
+    impact_size: string,
+    mark_price_band: string,
+    last_price_protected_band: string,
+    index_oracle_id: string,
+    expiry_time: string,
+    taker_fee: string,
+    maker_fee: string,
+    originator: string,
+  }
 }
 
 export interface SubmitProposalMsg<ProposalValue> {
