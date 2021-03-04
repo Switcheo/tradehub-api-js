5b6f09c111f9e9f24846687721839825db41dd48
Steven Khong <khongwh@gmail.com>
2021-03-04 13:46:05 +0800

    Add documentation to API parameters

--- src/lib/tradehub/api/get_account.ts
+++ src/lib/tradehub/api/get_account.ts
@@ -2,6 +2,10 @@ import { Account } from "@lib/models";
 import { CosmosResponse, TypedResponse } from "./util";
 
 export interface GetAccountResponse extends CosmosResponse<TypedResponse<Account>> { }
+
+/**
+ * @param address TradeHub address to query
+ */
 export interface GetAccountOpts {
   address?: string
 }
--- src/lib/tradehub/api/list_validator_delegations.ts
+++ src/lib/tradehub/api/list_validator_delegations.ts
@@ -2,6 +2,10 @@ import { Delegation } from "@lib/models";
 import { CosmosResponse } from "./util";
 
 export interface ListValidatorDelegationsResponse extends CosmosResponse<Delegation[]> { }
+
+/**
+ * @param validator TradeHub validator address to query (starts with `swthvaloper…`)
+ */
 export interface ListValidatorDelegationsOpts {
   validator: string
 }
