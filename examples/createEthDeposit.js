de9999ab37c590a9bf211ddf7ca8e7687145e141
Steven Khong <khongwh@gmail.com>
2021-03-05 11:24:01 +0800

    Update deposit script: use api to get token data

--- examples/createEthDeposit.js
+++ examples/createEthDeposit.js
@@ -1,34 +1,20 @@
-// const SDK = require('switcheo-chain-js-sdk') // use this instead if running this sdk as a library
-const { default: BigNumber } = require('bignumber.js')
+const BigNumber = require('bignumber.js')
 const { ethers } = require('ethers')
-const { ETHClient, Network, Blockchain } = require('../build/main')
-const SDK = require('../build/main')
-const { NETWORK } = require('../build/main/lib/config')
-const { clients } = SDK
-const { WalletClient } = clients
+const { ETHClient, Network, Blockchain, WalletClient, RestClient } = require('../build/main')
 const mnemonics = require('../mnemonics.json');
 
-(async () => {
-  const wallet = await WalletClient.connectMnemonic(mnemonics[0], Network.DevNet) // this is the receiving addr
-  const token = {
-    "name": "Switcheo 4 (BEP-20)",
-    "symbol": "SWTHB4",
-    "denom": "swth-b4",
-    "decimals": 8,
-    "blockchain": "bsc",
-    "chain_id": 79,
-    "asset_id": "7e0691323f7ad0be576f2ee98378271e84a15811",
-    "is_active": true,
-    "is_collateral": false,
-    "lock_proxy_hash": "12f466db69b85ee7aa17a66a8b75f8c6d6f04815",
-    "delegated_supply": "0",
-    "originator": "swth1xjad4uft0uk4d35nlk3gzp3v5gw5x2n40fmage",
+const network = Network.DevNet
+const TOKEN_DENOM = 'swth-b4';
 
-    // deposit amount update here
-    external_balance: new BigNumber(100).shiftedBy(8).toString(),
-  }
+(async () => {
+  const wallet = await WalletClient.connectMnemonic(mnemonics[0], network) // this is the receiving addr
+  const token = await new RestClient({ network, wallet }).getToken({ token: TOKEN_DENOM })
+
+  // deposit amount update here
+  token.external_balance = new BigNumber(100).shiftedBy(8).toString()
+
   const ethClient = ETHClient.instance({
-    network: Network.DevNet,
+    network,
     blockchain: Blockchain.BinanceSmartChain,
   })
 
@@ -54,7 +40,7 @@ const mnemonics = require('../mnemonics.json');
   })
   if (result === 'insufficient balance')
     throw new Error(result)
-  
+
   console.log('result', `${result.status} ${result.statusText}`)
 
   try {
