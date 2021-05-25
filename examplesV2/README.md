78e677b0b83ac9c9eeb2fad5fbda18e4e5453e6a
Ivan Poon <ravenxce@gmail.com>
2021-05-25 11:08:21 +0800

    Improve examples

--- examplesV2/README.md
+++ examplesV2/README.md
@@ -1,11 +1,17 @@
-e.g.
-```
-  ts-node ./examplesV2/get_account.ts
+# V2 Examples
+
+Either compile then run, or install `ts-node` and run using it.
+
+E.g.:
+
+```bash
+tsc ./examplesV2/get_account.ts
+node ./examplesV2/get_account.js
 ```
 
-
-You'll need `ts-node` installed
+or
 
 ```bash
 yarn global add typescript ts-node
+ts-node ./examplesV2/get_account.ts
 ```
--- examplesV2/create_token_proposal.ts
+++ examplesV2/create_token_proposal.ts
@@ -9,7 +9,7 @@ TradeHubSDK.APIClient.DEBUG_HEADERS = true;
   console.log("mnemonic:", mnemonic);
 
   const sdk = new TradeHubSDK({
-    network: TradeHubSDK.Network.TestNet,
+    network: TradeHubSDK.Network.LocalHost,
     debugMode: true,
   });
 
@@ -23,16 +23,16 @@ TradeHubSDK.APIClient.DEBUG_HEADERS = true;
     title: "test",
     description: "test",
     token: {
-      name: "Wrapped BNB",
-      symbol: "WBNB",
-      denom: "wbnb1",
-      decimals: 18 as any,
+      name: "Binance USD (BEP-20)",
+      symbol: "BUSD",
+      denom: "busd1",
+      decimals: "18",
       native_decimals: "18",
       blockchain: "bsc",
       chain_id: "6",
-      asset_id: "0x47837fC56f88F6A44Ed35c5e3F7981f6127dDe03",
+      asset_id: "e9e7cea3dedca5984780bafc599bd69add087d56",
       is_collateral: false,
-      lock_proxy_hash: "0x47837fC56f88F6A44Ed35c5e3F7981f6127dDe03",
+      lock_proxy_hash: "b5d4f343412dc8efb6ff599d790074d0f1e8d430",
       delegated_supply: "0",
       originator: connectedSDK.wallet.bech32Address,
     },
