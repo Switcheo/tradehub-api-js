2c89c133bdc3ff2d05205637bad6ad80abf0fea7
Steven Khong <khongwh@gmail.com>
2021-06-10 11:56:02 +0800

    Fix delegation API return type

--- src/lib/tradehub/api/spec/get_delegator_delegations.ts
+++ src/lib/tradehub/api/spec/get_delegator_delegations.ts
@@ -1,7 +1,7 @@
 import { RestModels } from "@lib/tradehub/models";
 import { CosmosResponse } from "./util";
 
-export interface GetDelegatorDelegationsResponse extends CosmosResponse<Array<RestModels.Delegation>> {}
+export interface GetDelegatorDelegationsResponse extends CosmosResponse<Array<RestModels.ValidatorDelegation>> {}
 
 export interface GetDelegatorDelegationsOpts {
     address: string
--- src/lib/tradehub/models/rest/validator/ValidatorDelegation.ts
+++ src/lib/tradehub/models/rest/validator/ValidatorDelegation.ts
@@ -3,6 +3,6 @@ import { TokenAmount } from '../token/TokenAmount'
 export interface ValidatorDelegation {
   delegator_address: string
   validator_address: string
-  shares: number
+  shares: string
   balance: TokenAmount
 }
