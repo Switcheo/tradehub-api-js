8ab2e30a67b9a3ec714f0ba6d3fec01cddad2ef4
Steven Khong <khongwh@gmail.com>
2021-06-22 10:24:13 +0800

    Refactor submit proposal api

--- src/lib/tradehub/sdk/modules/governance.ts
+++ src/lib/tradehub/sdk/modules/governance.ts
@@ -4,14 +4,14 @@ import { TxTypes } from "@lib/tradehub/utils";
 import BaseModule from "./module";
 
 class ModGovernance extends BaseModule {
-  public async submitProposal<T = RPCParams.Proposal>(type: string, deposit: TokenAmount[], proposer: string, params: T) {
+  public async submitProposal<T = RPCParams.Proposal>(type: string, params: T, deposit: TokenAmount[] = []) {
     const value: RPCParams.SubmitProposalMsg<T> = {
       content: {
         type,
         value: params,
       },
       initial_deposit: deposit,
-      proposer,
+      proposer: this.getWallet().bech32Address,
     };
     return await this.sendTx(
       TxTypes.Governance.SUBMIT_PROPOSAL,
