05ce894a0285a006e950cf9825c9f1edb272494b
Steven Khong <khongwh@gmail.com>
2021-06-30 17:25:50 +0800

    Refactor exports for util class

--- src/lib/tradehub/index.ts
+++ src/lib/tradehub/index.ts
@@ -1,6 +1,6 @@
 export { ZILClient, ZILClientOpts } from "./clients/index";
 export * from "./sdk";
 export { CosmosLedger } from "./utils";
+export * as TradeHubUtils from "./utils";
 export * from "./wallet";
 export * from "./websocket";
-
