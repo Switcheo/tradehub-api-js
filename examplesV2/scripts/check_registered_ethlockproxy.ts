23af926a29217cc6b1a43c25572fdd1ed17b1810
Steven Khong <khongwh@gmail.com>
2021-09-20 15:35:55 +0800

    Refactor check lp script

--- examplesV2/scripts/check_registered_ethlockproxy.ts
+++ examplesV2/scripts/check_registered_ethlockproxy.ts
@@ -1,37 +1,38 @@
 import { ethers } from "ethers";
 import { Blockchain, TradeHubSDK } from "../../build/main";
-import { ETHClient } from "../../build/main/lib/tradehub/clients/ETHClient";
-import { Network, NetworkConfigs } from "../../build/main/lib/tradehub/utils";
 import { ABIs } from "../../build/main/lib/eth";
+import { NetworkConfigs } from "../../build/main/lib/tradehub/utils";
+
+const RPC_URL = NetworkConfigs.mainnet.Eth.RpcURL; // "https://mainnet.infura.io/v3/";
+const CHECK_CHAIN = Blockchain.BinanceSmartChain;
 
 (async () => {
-  const network = Network.DevNet;
-  const ethClient = ETHClient.instance({
-    blockchain: Blockchain.Ethereum,
-    configProvider: {
-      getConfig: () => NetworkConfigs[network],
-    },
-  });
-  const lockProxyContract = new ethers.Contract(
-    ethClient.getLockProxyAddress(),
-    ABIs.lockProxy,
-    ethClient.getProvider()
-  );
   const sdk = await new TradeHubSDK({
-    network: network,
+    network: TradeHubSDK.Network.MainNet,
     debugMode: false,
   });
   await sdk.initialize()
   const tokensList = Object.values(sdk.token.tokens);
+  const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
   for (const token of tokensList) {
-    if (token.blockchain === 'eth') {
-      try {
-        const isRegistered = await lockProxyContract.registry(token.asset_id)
-        console.log(`${token.denom}: ${isRegistered}`)
-      } catch (error) {
-        console.log(`${token.denom}: ${error}`)
-      };
+    if (token.blockchain !== CHECK_CHAIN) {
+      continue;
     }
+
+    try {
+      const lockProxyContract = new ethers.Contract(
+        `0x${token.lock_proxy_hash}`,
+        ABIs.lockProxy,
+        provider,
+      );
+
+      const isRegistered = await lockProxyContract.registry(token.asset_id)
+
+      if (isRegistered === "0x0000000000000000000000000000000000000000000000000000000000000000")
+        console.log(`${token.denom}: ${isRegistered}`)
+    } catch (error) {
+      console.log(`${token.denom}: ${error}`)
+    };
   };
 })().catch(console.error).finally(() => process.exit(0))
