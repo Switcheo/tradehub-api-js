bfaf8c3ef149e7503a06344fbe6534757c9a6fcc
Steven Khong <khongwh@gmail.com>
2021-09-28 12:12:41 +0800

    v1.0.10

--- examplesV2/create_token.ts
+++ examplesV2/create_token.ts
@@ -4,19 +4,44 @@ import "./_setup";
 
 TradeHubSDK.APIClient.DEBUG_HEADERS = true;
 
-const PARAMS: CreateTokenMsg = {
-  name: 'Zilliqa USD',
-  symbol: 'zUSD',
-  denom: 'zusd',
-  decimals: '12',
-  native_decimals: '12',
+const PARAMS: CreateTokenMsg = [{
+  name: 'Zilliqa-bridged ETH',
+  symbol: 'zETH',
+  denom: 'eth.z.1',
+  decimals: '18',
+  native_decimals: '18',
   blockchain: 'zil',
-  chain_id: '110',
-  asset_id: 'ced1f00d5088ef3d246fc622e9b0e5173f2216bf',
+  chain_id: '18',
+  asset_id: '2ca315f4329654614d1e8321f9c252921192c5f2',
   is_collateral: false,
-  lock_proxy_hash: 'a5484b227f35f5e192e444146a3d9e09f4cdad80',
+  lock_proxy_hash: 'd73c6b871b4d0e130d64581993b745fc938a5be7',
   delegated_supply: '0',
-};
+}, {
+  name: 'Zilliqa-bridged USDT',
+  symbol: 'zUSDT',
+  denom: 'usdt.z.1',
+  decimals: '6',
+  native_decimals: '6',
+  blockchain: 'zil',
+  chain_id: '18',
+  asset_id: '5266b9a7bd7772c7ff596e1a7e5b4c49872ef232',
+  is_collateral: false,
+  lock_proxy_hash: 'd73c6b871b4d0e130d64581993b745fc938a5be7',
+  delegated_supply: '0',
+}, {
+  name: 'Zilliqa-bridged WBTC',
+  symbol: 'zWBTC',
+  denom: 'wbtc.z.1',
+  decimals: '8',
+  native_decimals: '8',
+  blockchain: 'zil',
+  chain_id: '18',
+  asset_id: '75fa7d8ba6bed4a68774c758a5e43cfb6633d9d6',
+  is_collateral: false,
+  lock_proxy_hash: 'd73c6b871b4d0e130d64581993b745fc938a5be7',
+  delegated_supply: '0',
+}];
+
 
 (async () => {
   const mnemonic = process.env.MNEMONICS ?? BIP39.generateMnemonic()
