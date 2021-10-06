496dc4ea4547812607da9b8492440c4ed9c295b7
Steven Khong <khongwh@gmail.com>
2021-10-06 18:58:54 +0800

    v1.0.19

--- examplesV2/connect_wallet.ts
+++ examplesV2/connect_wallet.ts
@@ -1,10 +1,11 @@
-import * as BIP39 from "bip39";
-import { TradeHubSDK } from '..';
+import { NEOAddress, TradeHubSDK } from '..';
 import "./_setup";
 
 (async () => {
-  const mnemonic = process.env.MNEMONICS ?? BIP39.generateMnemonic();
+  const mnemonic = "update evolve debate galaxy process trophy flame aspect abuse warfare scorpion reward"
   console.log("mnemonic:", mnemonic);
+
+  console.log(NEOAddress.generateAddress(mnemonic));
 
   const sdk = new TradeHubSDK({
     network: TradeHubSDK.Network.TestNet,
